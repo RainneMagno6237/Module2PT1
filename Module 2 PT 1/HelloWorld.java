ac635d1fd5b7f2be511b5fef0f75df30f09a51bb
RainneMagno6237 <rainnemagno@gmail.com>
2025-01-25 23:57:08 +0800

    Delete Module 2 PT 1 directory

--- Module 2 PT 1/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, my name is [Rainne]!");
-        System.out.println("I am excited to learn Java.");
-    }
-}
